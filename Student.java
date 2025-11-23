0ab3d034e4f25a574539ac42a2a60d8ed6d1995d
Shahzada <iloveyoutahseen@gmail.com>
2025-11-23 22:08:51 +0530

    Add Student class with details display method

---
+++ Student.java
@@ -0,0 +1,22 @@
+
+---
+
+### **📄 Student.java**
+```java
+public class Student {
+    private String name;
+    private int age;
+    private String course;
+
+    public Student(String name, int age, String course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
